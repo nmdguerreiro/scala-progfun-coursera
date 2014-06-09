598892b76be190b52e84da6145ba4de8338df948
Nuno Guerreiro <nmd.guerreiro@gmail.com>
2014-06-09 13:30:05 +0100

    Week 7 assignment

---
+++ week7/streams/.worksheet/src/test.scala
@@ -0,0 +1,4 @@
+import streams.BloxorzSuite
+object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(105); val res$0 = 
+  neighborsWithHistory(Block(Pos(1,1),Pos(1,1)), List(Left,Up));System.out.println("""res0: <error> = """ + $show(res$0))}
+}
